cbfea710379498f83d8280797b5e68bdd367d456
정예원 <yewonn.1j@gmail.com>
2022-07-27 17:39:36 +0900

    chore : recoil provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} /> {/* 왼쪽 하단 devtools */}
-			<Component {...pageProps} />
+			<RecoilRoot>
+				<ReactQueryDevtools initialIsOpen={false} /> {/* 왼쪽 하단 devtools */}
+				<Component {...pageProps} />
+			</RecoilRoot>
 		</QueryClientProvider>
 	);
 }
